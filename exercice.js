a5b0024c7944cf2400a2a9becb9c60d637fcd07b
remixxx31 <remiperray@gmail.com>
2022-05-28 01:06:01 +0200

    first commit

---
+++ exercice.js
@@ -0,0 +1,5 @@
+// Exercice 5
+// ON récupère la valeur du module dans une varaible
+var readline = require('readline-sync');
+var saisie = readline.question("Entrez un mois de l'année en chiffre")
+console.log("Vous avez saisi: " + saisie);
